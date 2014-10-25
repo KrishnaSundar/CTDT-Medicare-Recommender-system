928ae5f0edde49b36a2503ef221027f05264f3a7
Swagath <swagath1308@gmail.com>
2014-10-25 13:24:43 +0530

    [Home Page Changed -- Styles Modified]

---
+++ templates/Templates/web/bRec.php
@@ -0,0 +1,204 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>RecSys</title>
+		<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+		<script src="js/jquery.min.js"></script>
+		 <!-- Custom Theme files -->
+		<link href="css/style.css" rel='stylesheet' type='text/css' />
+   		 <!-- Custom Theme files -->
+   		 <meta name="viewport" content="width=device-width, initial-scale=1">
+		 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+   		 <!-- webfonts -->
+   		 <link href='http://fonts.googleapis.com/css?family=Arimo:400,700' rel='stylesheet' type='text/css'>
+   		  <!-- webfonts -->
+	</head>
+	<body>
+		<!-- container -->
+			<!-- header -->
+			
+			<div class="header header-border">
+				<div class="container">
+					<!-- logo -->
+					<div class="logo">
+						<a href="index.html"><img src="images/logo.png" title="medicalpluse" /></a>
+					</div>
+					<!-- logo -->
+					<!-- top-nav -->
+					<!--
+					<div class="top-nav">
+						<span class="menu"> </span>
+						<ul>
+							<li><a href="index.html">Home</a></li>
+							<li class="active"><a href="about.html">About</a></li>
+							<li><a href="services.html">Services</a></li>
+							<li><a href="blog.html">Blog</a></li>
+							<li><a href="contact.html">Contact</a></li>
+						</ul>
+					</div>
+					-->
+					<!-- top-nav -->
+						<!-- script-for-nav -->
+					<script>
+						$(document).ready(function(){
+							$("span.menu").click(function(){
+								$(".top-nav ul").slideToggle(300);
+							});
+						});
+					</script>
+					<!-- script-for-nav -->
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<!-- /header -->
+		</div>
+		<!-- /container -->
+		</div>
+		<!-- about-us -->
+		<div class="about">
+						<div class="container">
+							<div class="about-grids">
+								<div class="col-md-9 about-left">
+									<h1>What We Do</h1>
+									<p>Recommender Systems (RS) are software tools and techniques that provide suggestions and recommend items to be of use to a user.</p>
+									<p> These tools seek to predict the 'rating' or 'preference' a user would give to an item  or social elements that they had not yet considered using a model built on the characteristics of the item or user interaction environment. Becoming extremely common in recent years, the Recommender Systems can also be incorporated into elements of social importance. It is now one among the emerging topics today where the entire world needs some recommendations in each and every task they perform, thus making it the pivot in internet domain. The suggestions usually relate to various decision-making processes, such as what items to buy, what music to listen to, or what online news to read. RSs are primarily directed towards individuals who lack sufficient personal experience or competence to evaluate the potentially overwhelming number of alternatives available for them in the particular field they are searching. One such element is the area of health care which deserves alarming degree of attention at the moment. </p>
+									<p>The necessity of recommender systems in medicine seems viable for the future. This project provides a Trust-Enhanced recommender system which recommends its users based on the "Trust score". The data used by this recommender system being proposed here uses three objects Items, Users and Transactions (nothing but the interaction between the user and the system).</p>
+								</div>
+								<div class="col-md-3 about-left">
+									<h2>Latest News</h2>
+									<!-- news-grid -->
+									<div class="news-grid">
+										<img src="images/about-pic.jpg" title="name">
+									</div>
+									<!-- news-grid -->
+									<!-- recent-news -->
+									<div class="recent-posts">
+										<h2>Recent-posts</h2>
+										<!-- recent-post-grids -->
+											<div class="recent-post-grids">
+												<div class="r-post-grid">
+													<div class="r-post-grid-left">
+														<a href="#"><img src="images/d1.jpg" title="name"></a>
+													</div>
+													<div class="r-post-grid-right">
+														<h4>risus elit dignissim</h4>
+														<p>Dolor in commodo rutrum, risus elit dignissim mi,ligula eros nec nunc.</p>
+													</div>
+													<div class="clearfix"> </div>
+												</div>
+												<div class="r-post-grid">
+													<div class="r-post-grid-left">
+														<a href="#"><img src="images/d2.jpg" title="name"></a>
+													</div>
+													<div class="r-post-grid-right">
+														<h4>risus elit dignissim</h4>
+														<p>Dolor in commodo rutrum, risus elit dignissim mi,ligula eros nec nunc.</p>
+													</div>
+													<div class="clearfix"> </div>
+												</div>
+												<div class="r-post-grid">
+													<div class="r-post-grid-left">
+														<a href="#"><img src="images/d3.jpg" title="name"></a>
+													</div>
+													<div class="r-post-grid-right">
+														<h4>risus elit dignissim</h4>
+														<p>Dolor in commodo rutrum, risus elit dignissim mi,ligula eros nec nunc.</p>
+													</div>
+													<div class="clearfix"> </div>
+												</div>
+											</div>
+										<!-- recent-post-grids -->
+									</div>
+									<!-- recent-news -->
+								</div>
+							</div>
+						</div>
+					
+		<!-- about-us -->
+		<!-- team -->
+		<!-- I commented this
+		
+		<div class="team">
+						<div class="container">
+							<div class="team-head text-center">
+								<h3>Our team</h3>
+							</div>
+							<div class="team-grids">
+								<div class="col-md-4">
+									<div class="team-grid">
+										<div class="team-people">
+											<div class="team-people-pic">
+												<img src="images/d1.jpg" title="name">
+											</div>
+											<div class="team-people-info">
+												<h3>Established</h3>
+												<p>reader will be distracted by the readable</p>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="team-grid">
+										<div class="team-people">
+											<div class="team-people-pic">
+												<img src="images/d2.jpg" title="name">
+											</div>
+											<div class="team-people-info">
+												<h3>Established</h3>
+												<p>reader will be distracted by the readable</p>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="team-grid">
+										<div class="team-people">
+											<div class="team-people-pic">
+												<img src="images/d3.jpg" title="name">
+											</div>
+											<div class="team-people-info">
+												<h3>Established</h3>
+												<p>reader will be distracted by the readable</p>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="clearfix"> </div>
+							</div>
+						</div>
+					</div>
+		
+		</div>
+		-->
+		<!-- team -->
+
+		<!-- I commented this 
+		team-grids-caption 
+		<div class="team-grids-caption">
+								<div class="container">
+									<div class="team-grids-caption-left">
+										<h4>He point of using Lorem Ipsum is that</h4>
+										<p>as opposed to using Many desktop publishing packages and web page editors now use </p>
+									</div>
+									<div class="team-grids-caption-right">
+										<a class="team-btn" href="contact.html">Contact</a>
+									</div>
+									<div class="clearfix"> </div>
+								</div>
+							</div>
+		team-grids-caption -->
+		<!-- footer -->
+		<div class="footer">
+			<div class="container">
+			</div>
+		</div>
+		<!-- /footer -->
+	</body>
+</html>
+
